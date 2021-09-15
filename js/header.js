772a485e01fb359417c553c36f9924a8d586ef76
caiocastrofrs <caiocastrofrs@gmail.com>
2021-09-15 12:03:55 -0300

    arrumando animação do header no mobile

--- js/header.js
+++ js/header.js
@@ -1,24 +1,35 @@
 //FUNÇÃO PARA ATIVAR A ANIMAÇÃO DE EXPANDIR DO HEADER E A INSERÇÃO DOS DOIS LINKS 
 function expandirHeader() {
     let divHeader = document.getElementById('header');
-    setTimeout(async function() {
+    setTimeout(function() {
         divHeader.style.animationName = "animacaoExpandirHeader"; 
         divHeader.style.width = "33%";
          setTimeout(function() {
-            document.getElementById('equipe').style.opacity = "1";
-            document.getElementById('contato').style.opacity = "1";
+            document.getElementById('equipe').style.display = "inline-block";
+            document.getElementById('contato').style.display = "inline-block";
         },170)
     },100);
 }
 //FUNÇÃO PARA ESCONDER OS ITENS DO HEADER E DESATIVAR A ANIMAÇÃO
 function esconderHeader() {
     let divHeader = document.getElementById('header');
+    console.log(getComputedStyle(divHeader).width);
     setTimeout(function () {
         divHeader.style.animationName = "animacaoEsconderHeader";
         divHeader.style.width = "200px";
         setTimeout(function() {
-            document.getElementById('equipe').style.opacity = "0";
-            document.getElementById('contato').style.opacity = "0"; 
+            document.getElementById('equipe').style.display = "none";
+            document.getElementById('contato').style.display = "none"; 
         },150)
     },100);
+}
+
+const isMobile = () => window.matchMedia('(max-width: 700px)').matches
+window.onload = () => {
+    if(isMobile()){
+        let divHeader = document.getElementById('header');
+        divHeader.removeAttribute('onmouseenter');
+        divHeader.removeAttribute('onmouseleave');
+        divHeader.setAttribute('ontouchstart','expandirHeader()');
+    }
 }
